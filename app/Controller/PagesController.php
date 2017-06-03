e0140cfef69e42c638bd363815e7ecb65f0a42f8
Sudipto Das <dev.sudipto.das@gmail.com>
2017-06-04 00:14:35 +0600

    removed home.ctp

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -30,6 +30,12 @@ App::uses('AppController', 'Controller');
  */
 class PagesController extends AppController {
 
+    public function  beforeFilter()
+    {
+        $this->Auth->allow('display');
+    }
+
+
 /**
  * This controller does not use a model
  *
@@ -46,35 +52,6 @@ class PagesController extends AppController {
  *   or MissingViewException in debug mode.
  */
 	public function display() {
-		$path = func_get_args();
-
-		$count = count($path);
-		if (!$count) {
-			return $this->redirect('/');
-		}
-		if (in_array('..', $path, true) || in_array('.', $path, true)) {
-			throw new ForbiddenException();
-		}
-		$page = $subpage = $title_for_layout = null;
-
-		if (!empty($path[0])) {
-			$page = $path[0];
-		}
-		if (!empty($path[1])) {
-			$subpage = $path[1];
-		}
-		if (!empty($path[$count - 1])) {
-			$title_for_layout = Inflector::humanize($path[$count - 1]);
-		}
-		$this->set(compact('page', 'subpage', 'title_for_layout'));
-
-		try {
-			$this->render(implode('/', $path));
-		} catch (MissingViewException $e) {
-			if (Configure::read('debug')) {
-				throw $e;
-			}
-			throw new NotFoundException();
-		}
+        $this->layout = 'ajax';
 	}
 }
